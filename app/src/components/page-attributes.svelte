80d6aea6c74a867df1fa956d55fc02cb7f4cf02b
Ender <33337309+e3ndr@users.noreply.github.com>
2021-07-22 01:31:18 -0500

    Add an additional page attribute and a global helper.

--- app/src/components/page-attributes.svelte
+++ app/src/components/page-attributes.svelte
@@ -1,19 +1,45 @@
 <script>
     export let title = "";
+    export let allowNavigateBackwards = false;
 
 </script>
 
-<div class="hide">
+<div class="hidden">
     <!-- Page attributes -->
-    <span data-name="page-title">{title}</span>
-    <displayMode />
+    <span data-page-attr="title">{title}</span>
+    <span data-page-attr="allow-navigate-backwards" data-page-attr-type="boolean">{allowNavigateBackwards}</span>
 
     <script type="module">
         const currentPage = document.querySelector(".title-bar .current-page");
 
+        window.getPageAttribute = function (attributeName) {
+            const attrElement = document.querySelector(`[data-page-attr=${JSON.stringify(attributeName)}]`);
+
+            const value = attrElement?.innerText ?? null;
+            const type = attrElement?.getAttribute("data-page-attr-type") ?? "string";
+
+            if (value) {
+                switch (type) {
+                    case "boolean": {
+                        return value == "true";
+                    }
+
+                    case "number": {
+                        return parseFloat(value);
+                    }
+
+                    default: {
+                        return value;
+                    }
+                }
+            } else {
+                return null;
+            }
+        };
+
         // Grab the passed page title.
-        const pageTitle = document.querySelector("[data-name='page-title']").innerText;
-        if (pageTitle.length > 0) {
+        const pageTitle = getPageAttribute("title");
+        if (pageTitle && pageTitle.length > 0) {
             currentPage.innerText = `Caffeinated - ${pageTitle}`;
             document.title = `Casterlabs Caffeinated - ${pageTitle}`;
         } else {
