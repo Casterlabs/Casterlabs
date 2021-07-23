c0271f6a0aafa2981c6d95c2ebc3b16dfb482997
Ender <33337309+e3ndr@users.noreply.github.com>
2021-07-22 21:35:34 -0500

    app: Add a fade-on-hover css helper (and start using it).

--- app/src/components/settings/appearance-settings.svelte
+++ app/src/components/settings/appearance-settings.svelte
@@ -3,11 +3,6 @@
         color: inherit;
     }
 
-
-    #accessibility-zoom-reset:hover {
-        color: #7a7a7a !important;
-    }
-
     #accessibility-zoom-reset :global(svg) {
         height: 20px;
         width: 20px;
@@ -49,7 +44,7 @@
             Zoom
             <br />
             <input id="accessibility-zoom" type="range" min="-2" max="2" step=".01" list="accessibility-zoom-list" />
-            <a id="accessibility-zoom-reset" class="hidden">
+            <a id="accessibility-zoom-reset" class="hidden fade-on-hover">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-rotate-ccw">
                     <polyline points="1 4 1 10 7 10"></polyline>
                     <path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"></path>
--- app/src/routes/settings.svelte
+++ app/src/routes/settings.svelte
@@ -47,10 +47,6 @@
         width: 28px;
     }
 
-    .settings-close:hover {
-        color: #7a7a7a !important;
-    }
-
     /* Pad out the settings page */
     :global(.settings-navigate) {
         padding-top: 2em;
@@ -62,7 +58,7 @@
 </style>
 
 <div class="settings-contents no-select">
-    <a class="settings-close" onclick="history.back()">
+    <a class="settings-close fade-on-hover" onclick="history.back()">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle">
             <circle cx="12" cy="12" r="10"></circle>
             <line x1="15" y1="9" x2="9" y2="15"></line>
