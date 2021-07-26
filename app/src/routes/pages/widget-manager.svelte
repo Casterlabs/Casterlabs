3246785a34e24a5d7e757bea7a45f77514fd0233
Ender <33337309+e3ndr@users.noreply.github.com>
2021-07-25 23:24:26 -0500

    app: Fill in those placeholder pages.

---
+++ app/src/routes/pages/widget-manager.svelte
@@ -0,0 +1,15 @@
+<script>
+    import PageAttributes from "../components/page-attributes.svelte";
+    import HideSideBar from "../components/displaymodes/hide-sidebar.svelte";
+</script>
+
+<PageAttributes />
+<HideSideBar />
+
+<div class="has-text-centered">
+    <br />
+    <br />
+    <h1 class="title">
+        test page :^)
+    </h1>
+</div>
