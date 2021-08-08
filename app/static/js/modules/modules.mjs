b24f886d88566f863652e810bebbb5b34f0b0bb6
Ender <33337309+e3ndr@users.noreply.github.com>
2021-08-07 22:25:02 -0500

    app: Get dynamic modules working

--- app/static/js/modules/modules.mjs
+++ app/static/js/modules/modules.mjs
@@ -1,13 +1,16 @@
 import ModuleInstance from "./module-instance.mjs";
 import { moduleStore } from "../caffeinated.mjs";
+import { generateUUID } from "../util/misc.mjs";
 
-let registeredDynamicModules = {};
+let dynamicModuleHolders = {};
 let dynamicModules = {};
 
 let staticModules = {};
 
-function createModule(baseUrl, isStatic, moduleDeclaration) {
-    const { namespace, id, name, location, settings } = moduleDeclaration;
+let loadedRepos = {};
+
+function createModule(baseUrl, isStatic, moduleDeclaration, name, id) {
+    const { namespace, location, settings } = moduleDeclaration;
     const fullId = `${namespace}:${id}`;
 
     if (staticModules[fullId] || dynamicModules[fullId]) {
@@ -28,26 +31,123 @@ function createModule(baseUrl, isStatic, moduleDeclaration) {
 
         const instance = new ModuleInstance(namespace, id, name, `${baseUrl}/${location}`, storedSettings, defaultSettings);
 
+        instance.destroyHandlers.push(() => {
+            if (isStatic) {
+                delete staticModules[fullId];
+            } else {
+                delete dynamicModules[fullId];
+            }
+        });
+
         if (isStatic) {
             staticModules[fullId] = instance;
         } else {
             dynamicModules[fullId] = instance;
         }
+
+        return instance;
     }
 }
 
 async function registerRepo(baseUrl) {
-    const modulesManifest = await (await fetch(`${baseUrl}/modules.json`)).json();
+    if (loadedRepos[baseUrl]) {
+        throw "Repo is already loaded."
+    } else {
+        const modulesManifest = await (await fetch(`${baseUrl}/modules.json`)).json();
 
-    for (const staticModule of modulesManifest.static) {
-        createModule(baseUrl, true, staticModule);
+        let _staticModules = [];
+        let _dynamicModuleHolders = [];
+
+        for (const declaration of modulesManifest.dynamic) {
+            if (dynamicModuleHolders[declaration.namespace]) {
+                throw `Dynamic module holder of namespace "${declaration.namespace}" is already registered.`
+            } else {
+                const holder = new DynamicModuleHolder(declaration, baseUrl);
+
+                _dynamicModuleHolders.push(holder);
+                dynamicModuleHolders[holder.namespace] = holder;
+            }
+        }
+
+        for (const staticModule of modulesManifest.static) {
+            const instance = createModule(baseUrl, true, staticModule, staticModule.name, staticModule.id);
+            _staticModules.push(instance);
+        }
+
+        const repoInstance = new RepoInstance(baseUrl, _dynamicModuleHolders, _staticModules);
+
+        loadedRepos[baseUrl] = repoInstance;
+
+        return repoInstance;
     }
-
-    // TODO DYNAMIC
 }
 
-function getRegisteredDynamicModules() {
-    return registeredDynamicModules;
+class DynamicModuleHolder {
+    #declaration = null;
+    #baseUrl = null;
+    #runningModules = {};
+
+    constructor(declaration, baseUrl) {
+        this.#declaration = declaration;
+        this.#baseUrl = baseUrl;
+    }
+
+    get namespace() {
+        return this.#declaration.namespace;
+    }
+
+    create(name, id = generateUUID()) {
+        const instance = createModule(this.#baseUrl, false, this.#declaration, name, id);
+
+        this.#runningModules[id] = instance;
+
+        instance.destroyHandlers.push(() => {
+            delete this.#runningModules[id];
+        });
+    }
+
+    destroyAll() {
+        for (const dynamic of Object.values(this.#runningModules)) {
+            dynamic.destroy();
+        }
+
+        delete dynamicModuleHolders[this.namespace]
+    }
+
+}
+
+class RepoInstance {
+    #baseUrl = null;
+
+    #dynamicModuleHolders = null;
+    #staticModules = null;
+
+    constructor(baseUrl, dynamicModuleHolders, staticModules) {
+        this.#baseUrl = baseUrl;
+        this.#dynamicModuleHolders = dynamicModuleHolders;
+        this.#staticModules = staticModules;
+    }
+
+    get baseUrl() {
+        return this.#baseUrl;
+    }
+
+    async destroy() {
+        for (const module of this.#staticModules) {
+            module.destroy();
+        }
+
+        for (const module of this.#dynamicModuleHolders) {
+            module.destroyAll();
+        }
+
+        delete loadedRepos[this.baseUrl];
+    }
+
+}
+
+function getDynamicModuleHolders() {
+    return dynamicModuleHolders;
 }
 
 function getDynamicModules() {
@@ -58,9 +158,14 @@ function getStaticModules() {
     return staticModules;
 }
 
+function getLoadedRepos() {
+    return loadedRepos;
+}
+
 export {
     registerRepo,
-    getRegisteredDynamicModules,
+    getDynamicModuleHolders,
     getDynamicModules,
-    getStaticModules
+    getStaticModules,
+    getLoadedRepos
 };
