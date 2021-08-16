7dd6e403cbc72e5df6207311856841517af09b98
Ender <33337309+e3ndr@users.noreply.github.com>
2021-08-16 05:56:21 -0500

    default-modules: Remove the alert test, it got annoying.

--- default-modules/widgets/chat/widget.js
+++ default-modules/widgets/chat/widget.js
@@ -3,5 +3,5 @@ Module.on("init", ({ name, id, namespace }) => {
     console.debug("My name:", name);
     console.debug("Location:", import.meta.url)
     console.debug(Caffeinated)
-    alert("Hello from the Chat Widget!")
+    // alert("Hello from the Chat Widget!")
 });
