b89de9308e515fdb77917ced18d340374baffc9a
Ender <33337309+e3ndr@users.noreply.github.com>
2021-08-17 07:08:03 -0500

    app: Begin some work on the edit widget page.

---
+++ app/src/routes/pages/edit-widget.svelte
@@ -0,0 +1,65 @@
+<script>
+    import PageAttributes from "../../components/page-attributes.svelte";
+    import HideSideBar from "../../components/displaymodes/hide-sidebar.svelte";
+
+    import { onMount } from "svelte";
+
+    let widgetSettings = [];
+
+    onMount(async () => {
+
+    });
+</script>
+
+<style>
+
+</style>
+
+<PageAttributes allowNavigateBackwards="true" title="Widget Manager" />
+<HideSideBar />
+
+<div class="has-text-left">
+    {#each widgetSettings as widgetCategory}
+
+    <!-- 
+    {
+        "name": "chat_direction",
+        "label": "caffeinated.chat.chat_direction",
+        "items": [
+            {
+                "name": "chat_direction",
+                "type": "select",
+                "defaultValue": "down",
+                "options": {
+                    "down": "caffeinated.chat.chat_direction.down",
+                    "up": "caffeinated.chat.chat_direction.up"
+                }
+            }
+        ]
+    }
+    -->
+
+    <div>
+        {#each widgetCategory.items as widgetSettingsOption}
+
+        <!--
+            {
+                "name": "chat_direction",
+                "type": "select",
+                "defaultValue": "down",
+                "options": {
+                    "down": "caffeinated.chat.chat_direction.down",
+                    "up": "caffeinated.chat.chat_direction.up"
+                }
+            }
+        -->
+
+        {/each}
+    </div>
+
+    <script>
+        feather.replace();
+    </script>
+    {/each}
+
+</div>
