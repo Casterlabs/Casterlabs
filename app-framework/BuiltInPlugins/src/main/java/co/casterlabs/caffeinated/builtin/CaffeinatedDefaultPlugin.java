8ce0715f42a57bfbbaeb66266871940e81fc8132
Ender <33337309+e3ndr@users.noreply.github.com>
2021-11-28 18:59:17 -0600

    Framework: Cleanup the default chat widget code a teeny bit.

--- app-framework/BuiltInPlugins/src/main/java/co/casterlabs/caffeinated/builtin/CaffeinatedDefaultPlugin.java
+++ app-framework/BuiltInPlugins/src/main/java/co/casterlabs/caffeinated/builtin/CaffeinatedDefaultPlugin.java
@@ -3,9 +3,8 @@ package co.casterlabs.caffeinated.builtin;
 import co.casterlabs.caffeinated.builtin.widgets.ChatWidget;
 import co.casterlabs.caffeinated.pluginsdk.CaffeinatedPlugin;
 import co.casterlabs.caffeinated.pluginsdk.PluginImplementation;
-import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetDetails;
-import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetDetails.WidgetDetailsCategory;
 import lombok.NonNull;
+import xyz.e3ndr.fastloggingframework.logging.FastLogger;
 
 @PluginImplementation
 public class CaffeinatedDefaultPlugin extends CaffeinatedPlugin {
@@ -14,15 +13,15 @@ public class CaffeinatedDefaultPlugin extends CaffeinatedPlugin {
     public void onInit() {
         this.getLogger().info("I am loaded!");
 
-        this.getPlugins().registerWidget(
-            this,
-            new WidgetDetails()
-                .withNamespace("co.casterlabs.chat")
-                .withIcon("message-square")
-                .withCategory(WidgetDetailsCategory.INTERACTION)
-                .withFriendlyName("Chat Widget"),
-            ChatWidget.class
-        );
+        this.getPlugins().registerWidget(this, ChatWidget.DETAILS, ChatWidget.class);
+
+        // I spend way too long on this shit.
+        FastLogger.logStatic(" _____________");
+        FastLogger.logStatic("|     Hi!     |");
+        FastLogger.logStatic("| My name is: |");
+        FastLogger.logStatic("|‾‾‾‾‾‾‾‾‾‾‾‾‾|");
+        FastLogger.logStatic("| %-11s |", this.getName());
+        FastLogger.logStatic("|_____________|");
     }
 
     @Override
--- app-framework/BuiltInPlugins/src/main/java/co/casterlabs/caffeinated/builtin/widgets/ChatWidget.java
+++ app-framework/BuiltInPlugins/src/main/java/co/casterlabs/caffeinated/builtin/widgets/ChatWidget.java
@@ -1,46 +1,45 @@
 package co.casterlabs.caffeinated.builtin.widgets;
 
 import co.casterlabs.caffeinated.pluginsdk.widgets.Widget;
+import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetDetails;
+import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetDetails.WidgetDetailsCategory;
 import co.casterlabs.caffeinated.pluginsdk.widgets.settings.WidgetSettingsItem;
 import co.casterlabs.caffeinated.pluginsdk.widgets.settings.WidgetSettingsLayout;
 import co.casterlabs.caffeinated.pluginsdk.widgets.settings.WidgetSettingsSection;
-import xyz.e3ndr.fastloggingframework.logging.FastLogger;
 
 public class ChatWidget extends Widget {
+    public static final WidgetDetails DETAILS = new WidgetDetails()
+        .withNamespace("co.casterlabs.chat")
+        .withIcon("message-square")
+        .withCategory(WidgetDetailsCategory.INTERACTION)
+        .withFriendlyName("Chat Widget");
+
+    private static final WidgetSettingsLayout LAYOUT = new WidgetSettingsLayout()
+        .addSection(
+            new WidgetSettingsSection("chat_style", "Style")
+                .addItem(WidgetSettingsItem.asDropdown("chat_direction", "Chat Direction", "down", "down", "up"))
+                .addItem(WidgetSettingsItem.asUnknown("font", "Font", "Poppins"))
+                .addItem(WidgetSettingsItem.asNumber("font_size", "Font Size", 16, 1, 0, 128))
+                .addItem(WidgetSettingsItem.asColor("text_color", "Text Color", "#ffffff"))
+                .addItem(WidgetSettingsItem.asDropdown("text_align", "Text Align", "left", "left", "right"))
+                .addItem(WidgetSettingsItem.asCheckbox("show_donations", "Show Donations", true))
+        )
+        .addSection(
+            new WidgetSettingsSection("custom_emotes", "Custom Emotes")
+                .addItem(WidgetSettingsItem.asCheckbox("show_betterbrime", "Show BetterBrime Emotes", true))
+                .addItem(WidgetSettingsItem.asCheckbox("show_betterttv", "Show BetterTTV Emotes", true))
+                .addItem(WidgetSettingsItem.asCheckbox("show_casterlabsemotes", "Show Casterlabs Emotes", true))
+        )
+        .addSection(
+            new WidgetSettingsSection("moderation", "Moderation")
+                .addItem(WidgetSettingsItem.asCheckbox("hide_bots", "Hide Bots", true))
+                .addItem(WidgetSettingsItem.asCheckbox("hide_naughty_language", "Hide Naughty Language", true))
+        );
 
     @Override
     public void onInit() {
-        // I spend way too long on this shit.
-        FastLogger.logStatic(" _____________");
-        FastLogger.logStatic("|     Hi!     |");
-        FastLogger.logStatic("| My name is: |");
-        FastLogger.logStatic("|‾‾‾‾‾‾‾‾‾‾‾‾‾|");
-        FastLogger.logStatic("| %-11s |", this.getName());
-        FastLogger.logStatic("|_____________|");
+        this.setSettingsLayout(LAYOUT);
 
-        this.setSettingsLayout(
-            new WidgetSettingsLayout()
-                .addSection(
-                    new WidgetSettingsSection("chat_style", "Style")
-                        .addItem(WidgetSettingsItem.asDropdown("chat_direction", "Chat Direction", "down", "down", "up"))
-                        .addItem(WidgetSettingsItem.asUnknown("font", "Font", "Poppins"))
-                        .addItem(WidgetSettingsItem.asNumber("font_size", "Font Size", 16, 1, 0, 128))
-                        .addItem(WidgetSettingsItem.asColor("text_color", "Text Color", "#ffffff"))
-                        .addItem(WidgetSettingsItem.asDropdown("text_align", "Text Align", "left", "left", "right"))
-                        .addItem(WidgetSettingsItem.asCheckbox("show_donations", "Show Donations", true))
-                )
-                .addSection(
-                    new WidgetSettingsSection("custom_emotes", "Custom Emotes")
-                        .addItem(WidgetSettingsItem.asCheckbox("show_betterbrime", "Show BetterBrime Emotes", true))
-                        .addItem(WidgetSettingsItem.asCheckbox("show_betterttv", "Show BetterTTV Emotes", true))
-                        .addItem(WidgetSettingsItem.asCheckbox("show_casterlabsemotes", "Show Casterlabs Emotes", true))
-                )
-                .addSection(
-                    new WidgetSettingsSection("moderation", "Moderation")
-                        .addItem(WidgetSettingsItem.asCheckbox("hide_bots", "Hide Bots", true))
-                        .addItem(WidgetSettingsItem.asCheckbox("hide_naughty_language", "Hide Naughty Language", true))
-                )
-        );
     }
 
 }
