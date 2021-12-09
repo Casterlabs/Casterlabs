68de424faa138f913edc8265c50b9231c9b6e986
Ender <33337309+e3ndr@users.noreply.github.com>
2021-12-08 23:21:13 -0600

    Windows building, woot!

---
+++ app-framework/Build/Caffeinated-Windows/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Let's get it started.
+cd Build/Caffeinated-Windows
+
+echo "Building for Windows..."
+
+JRE_DOWNLOAD_URL="https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u312-b07/OpenJDK8U-jre_x64_windows_hotspot_8u312b07.zip"
+MAIN_CLASS="co.casterlabs.caffeinated.bootstrap.Bootstrap"
+
+rm -rf target/dist
+
+if [ ! -f runtime.zip ]; then
+    echo "Downloading JRE from ${JRE_DOWNLOAD_URL}."
+    wget -O runtime.zip $JRE_DOWNLOAD_URL
+fi
+
+java -jar "../packr.jar" \
+     --platform windows64 \
+     --jdk runtime.zip \
+     --executable Casterlabs-Caffeinated \
+     --classpath target/jcef-bundle-win64/Caffeinated-Windows.jar \
+     --mainclass $MAIN_CLASS \
+     --resources target/jcef-bundle-win64/cef_bundle \
+     --output target/dist
+
+# Copy the files.
+mkdir -p ../../dist
+cp -r target/dist ../../dist/windows
+
+# Return us back to the root dir.
+cd ..
+cd ..
+
+echo "Finished building for Windows."
---
+++ app-framework/build.sh
@@ -0,0 +1,9 @@
+rm -rf dist/*
+
+# mvn clean package
+
+echo ""
+echo "Completing packaging of application."
+echo ""
+
+sh Build/Caffeinated-Windows/build.sh
