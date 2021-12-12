b5902d6e7efef2f5c354bb6b00f8dd8adbf0bd19
Ender <33337309+e3ndr@users.noreply.github.com>
2021-12-12 01:06:59 -0600

    Framework: Cleanup the build info.

--- app-framework/App/src/main/java/co/casterlabs/caffeinated/app/BuildInfo.java
+++ app-framework/App/src/main/java/co/casterlabs/caffeinated/app/BuildInfo.java
@@ -1,6 +1,7 @@
 package co.casterlabs.caffeinated.app;
 
 import co.casterlabs.rakurai.json.annotating.JsonClass;
+import co.casterlabs.rakurai.json.validation.JsonValidate;
 import lombok.Getter;
 
 @Getter
@@ -9,9 +10,17 @@ public class BuildInfo {
     private String author;
     private String version;
     private String buildChannel;
+    private String versionString;
 
-    public String getVersionString() {
-        return String.format("%s-%s", this.version, this.buildChannel);
+    @JsonValidate
+    private void validate() {
+        if (this.version.startsWith("${")) {
+            this.buildChannel = "dev";
+            this.version = "0.0.0";
+            this.versionString = "dev-build";
+        } else {
+            this.versionString = String.format("%s-%s", this.version, this.buildChannel);
+        }
     }
 
 }
