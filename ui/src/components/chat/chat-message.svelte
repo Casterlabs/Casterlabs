eab05421831b1d1c195c9c46229d53e3edcc25dd
Ender <33337309+e3ndr@users.noreply.github.com>
2021-12-12 23:10:45 -0600

    Begin to make the chat viewer s e x y

--- ui/src/components/chat/chat-message.svelte
+++ ui/src/components/chat/chat-message.svelte
@@ -1,47 +1,101 @@
+<svelte:options accessors />
+
 <script>
     import User from "./user.svelte";
 
-    export let chatEvent = null;
+    export let koiEvent = null;
     export let isMultiPlatform = false;
+    export let isDeleted = false;
+    export let upvotes = koiEvent.upvotes || 0;
+    export const timestamp = Date.now();
+
+    export let sender = null; // We set this.
+    let messageHtml = "";
+
+    let highlight = false;
 
     function escapeHtml(unsafe) {
         return unsafe.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     }
 
-    let messageHtml = escapeHtml(chatEvent.message);
-    let upvotesHtml = chatEvent.upvotes == 0 ? "" : chatEvent.upvotes;
+    if (koiEvent.event_type == "CLEARCHAT") {
+        highlight = true;
+        messageHtml = `<i>Chat was cleared.</i>`;
+    } else if (["CHAT", "DONATION"].includes(koiEvent.event_type) /* Normal chat messages */) {
+        messageHtml = escapeHtml(koiEvent.message);
+        sender = koiEvent.sender;
 
-    for (const [name, image] of Object.entries(chatEvent.emotes)) {
-        messageHtml = messageHtml.split(name).join(`<img class="inline-image" title="${name}" src="${image}" />`);
-    }
-
-    for (const pattern of chatEvent.links) {
-        const link = pattern.includes("://") ? pattern : "https://" + pattern;
-
-        messageHtml = messageHtml.split(pattern).join(`<a href="${link}" rel="external">${pattern}</a>`);
-    }
-
-    if (chatEvent.donations) {
-        // Caffeine Props & Trovo Spells don't appear in chat like Twitch Cheers do.
-        for (const donation of chatEvent.donations) {
-            messageHtml += ` <img class="inline-image" src="${donation.image}" />`;
+        for (const [name, image] of Object.entries(koiEvent.emotes)) {
+            messageHtml = messageHtml.split(name).join(`<img class="inline-image" title="${name}" src="${image}" />`);
         }
+
+        for (const pattern of koiEvent.links) {
+            const link = pattern.includes("://") ? pattern : "https://" + pattern;
+
+            messageHtml = messageHtml.split(pattern).join(`<a href="${link}" rel="external">${pattern}</a>`);
+        }
+
+        if (koiEvent.donations) {
+            // Caffeine Props & Trovo Spells don't appear in chat like Twitch Cheers do.
+            for (const donation of koiEvent.donations) {
+                messageHtml += ` <img class="inline-image" src="${donation.image}" />`;
+            }
+        }
+    } else if (koiEvent.event_type == "CLEARCHAT") {
     }
 </script>
 
-<li>
-    <span data-id={chatEvent.id} data-sender={chatEvent.sender.UPID}>
-        <User {isMultiPlatform} userData={chatEvent.sender} />
-        {messageHtml}<sup class="upvote-counter upvote-1">
-            {upvotesHtml}
+<span class="chat-message {isDeleted ? 'is-deleted' : ''} {highlight ? 'highlighted' : ''}">
+    {#if sender}
+        <User {isMultiPlatform} userData={sender} />
+    {/if}
+
+    {@html messageHtml}{#if upvotes > 0}
+        <sup class="upvote-counter">
+            {#if upvotes < 10}
+                <span class="upvote-1">{upvotes}</span>
+            {:else if upvotes < 100}
+                <span class="upvote-2">{upvotes}</span>
+            {:else if upvotes < 1000}
+                <span class="upvote-3">{upvotes}</span>
+            {:else}
+                <span class="upvote-4">{upvotes}</span>
+            {/if}
         </sup>
-    </span>
-</li>
+    {/if}
+</span>
 
 <style>
     :global(.inline-image) {
         height: 1.3em;
         vertical-align: text-bottom;
+    }
+
+    .chat-message {
+        padding-left: 10px;
+        display: block;
+        width: 100%;
+    }
+
+    .highlighted {
+        margin-top: 15px;
+        margin-bottom: 15px;
+        text-align: center;
+        background-color: rgba(0, 0, 0, 0.15);
+    }
+
+    .is-deleted {
+        filter: grayscale(0.45);
+        transition: 0.15s;
+    }
+
+    .is-deleted:hover {
+        filter: grayscale(0);
+        transition: 0.15s;
+    }
+
+    .upvote-counter {
+        text-shadow: 1px 1px rgba(0, 0, 0, 0.75);
     }
 
     /* Upvotes */
--- ui/src/routes/pages/stream-chat.svelte
+++ ui/src/routes/pages/stream-chat.svelte
@@ -5,10 +5,15 @@
 
     import { onMount, onDestroy } from "svelte";
 
+    const DISPLAYABLE_EVENTS = ["FOLLOW", "CHAT", "DONATION", "SUBSCRIPTION", "VIEWER_JOIN", "VIEWER_LEAVE", "RAID", "CHANNEL_POINTS", "CLEARCHAT"];
+
     let eventHandler;
 
-    let chatHistory = [];
+    let chatHistory = {};
     let isMultiPlatform = true;
+    let chatbox;
+
+    let chatSettingsOpen = false;
 
     setPageProperties({
         showSideBar: true,
@@ -17,47 +22,69 @@
     });
 
     function onMeta(event) {
-        const chatElement = chatBox.querySelector(`[data-id='${event.id}']`);
+        const chatElement = chatHistory[event.id];
 
-        if (event.is_visible) {
-            const counterElement = chatElement.querySelector(".upvote-counter");
-
-            if (event.upvotes > 0) {
-                counterElement.innerText = event.upvotes;
-
-                if (event.upvotes >= 1000) {
-                    counterElement.classList = "upvote-4";
-                } else if (event.upvotes >= 100) {
-                    counterElement.classList = "upvote-3";
-                } else if (event.upvotes >= 10) {
-                    counterElement.classList = "upvote-2";
-                } else if (event.upvotes >= 1) {
-                    counterElement.classList = "upvote-1";
-                }
-            }
-        } else {
-            chatElement.remove();
-        }
+        chatElement.upvotes = event.upvotes;
+        chatElement.isDeleted = !event.is_visible;
     }
 
     // TODO better processing of messages, this works for now.
     function processEvent(event) {
-        console.log(event);
+        if (event.event_type == "META") {
+            onMeta(event);
+        }
 
-        switch (event.event_type) {
-            case "CHAT":
-            case "DONATION": {
-                chatHistory.push(event);
-                break;
-            }
+        // This means that we must delete ALL of the messages from a user.
+        else if (event.event_type == "CLEARCHAT" && event.user_upid) {
+            for (const chatMessage of Object.values(chatHistory)) {
+                const koiEvent = chatMessage.koiEvent;
 
-            case "META": {
-                onMeta(event);
-                break;
+                if (koiEvent.sender && koiEvent.sender.UPID == event.user_upid) {
+                    chatMessage.isDeleted = true;
+                }
             }
         }
 
-        chatHistory = chatHistory; // Svelte update.
+        // Display the chat message.
+        else if (DISPLAYABLE_EVENTS.includes(event.event_type)) {
+            // Mark old messages as deleted.
+            // We do still need to create the message to let the user know chat was cleared.
+            if (event.event_type == "CLEARCHAT") {
+                const now = Date.now();
+
+                for (const chatMessage of Object.values(chatHistory)) {
+                    if (chatMessage.timestamp < now) {
+                        chatMessage.isDeleted = true;
+                    }
+                }
+            }
+
+            const elem = document.createElement("li");
+
+            const message = new ChatMessage({
+                target: elem,
+                props: {
+                    koiEvent: event,
+                    isMultiPlatform: isMultiPlatform
+                }
+            });
+
+            if (event.id) {
+                // This event is editable in some way, shape, or form.
+                // (so, we must keep track of it)
+                chatHistory[event.id] = message;
+            }
+
+            chatbox.appendChild(elem);
+        } else {
+            return;
+        }
+
+        console.log("[StreamChat]", "Processed event:", event);
+    }
+
+    function toggleChatSettings() {
+        chatSettingsOpen = !chatSettingsOpen;
     }
 
     onDestroy(() => {
@@ -67,27 +94,56 @@
     onMount(async () => {
         eventHandler = Bridge.createThrowawayEventHandler();
 
+        window.test = chatHistory;
+
         eventHandler.on("koi:event", processEvent);
-        (await Bridge.query("koi")).data.history.forEach(processEvent);
+        (await Bridge.query("koi:history")).data.forEach(processEvent);
     });
 </script>
 
-<div class="stream-chat-container">
-    <div class="chat-content">
-        <div id="chat-box" class="allow-select">
-            <ul>
-                {#each chatHistory as chatEvent}
-                    <ChatMessage {chatEvent} bind:isMultiPlatform />
-                {/each}
-            </ul>
+<div class="stream-chat-container {chatSettingsOpen ? 'chat-settings-open' : ''}">
+    <div id="chat-box" class="allow-select">
+        <ul bind:this={chatbox} />
+    </div>
+
+    <div id="chat-settings" class="box">some settings here.</div>
+
+    <div id="interact-box">
+        <div class="field has-addons">
+            <div class="control is-expanded" style="position: relative;">
+                <input class="input" type="text" placeholder="Send a message" />
+
+                <!-- svelte-ignore a11y-missing-attribute -->
+                <a class="chat-settings-button highlight-on-hover" on:click={toggleChatSettings}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="white"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="feather feather-settings"
+                    >
+                        <circle cx="12" cy="12" r="3" />
+                        <path
+                            d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"
+                        />
+                    </svg>
+                </a>
+            </div>
+            <div class="control">
+                <button class="button"> Send </button>
+            </div>
         </div>
     </div>
-    <div id="interact-box" />
 </div>
 
 <style>
     :root {
-        --interact-height: 30px;
+        --interact-height: 40.5px;
+        --interact-top-margin: 12px;
+        --interact-bottom-margin: 25px;
     }
 
     .stream-chat-container {
@@ -98,26 +154,66 @@
         right: 0;
     }
 
-    .chat-content {
-        height: 100%;
-    }
-
     #chat-box {
         position: absolute;
         top: 0;
-        bottom: var(--interact-height);
+        bottom: calc(var(--interact-top-margin) + var(--interact-height) + var(--interact-bottom-margin));
         left: 0;
         right: 0;
         font-size: 1.05em;
-        margin-top: 5px;
-        margin-left: 10px;
+        padding-top: 10px;
+        overflow-y: auto;
+        overflow-x: hidden;
     }
 
     #interact-box {
         position: absolute;
-        bottom: 0;
-        left: 0;
-        right: 0;
+        bottom: var(--interact-bottom-margin);
+        left: 15px;
+        right: 15px;
         height: var(--interact-height);
+    }
+
+    /* Chat settings */
+
+    .chat-settings-button {
+        position: absolute;
+        top: 5px;
+        right: 10px;
+        z-index: 20;
+        width: 30px;
+        height: 30px;
+        transition: 0.15s;
+        border-radius: 5px;
+        filter: invert(var(--white-invert-factor));
+    }
+
+    .chat-settings-button svg {
+        height: 20px;
+        width: 20px;
+        margin-left: calc(50% - 10px);
+        margin-top: calc(50% - 10px);
+        transition: 0.35s;
+    }
+
+    .chat-settings-open .chat-settings-button svg {
+        transform: rotate(45deg);
+    }
+
+    #chat-settings {
+        position: absolute;
+        right: 25px;
+        bottom: calc(var(--interact-top-margin) + var(--interact-height));
+        height: 0px;
+        width: 200px;
+        visibility: hidden;
+        opacity: 0;
+        transition: 0.35s;
+    }
+
+    .chat-settings-open #chat-settings {
+        visibility: visible;
+        height: 300px;
+        opacity: 1;
     }
 </style>
