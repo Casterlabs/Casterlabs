227065341afda1619b4e535c6d93fa9147cd6b6a
Ender <33337309+e3ndr@users.noreply.github.com>
2021-12-27 22:28:53 -0600

    App: Add the RecentSubscriberLabel, upcoming commit will add a feature.

---
+++ app-framework/BuiltInPlugins/src/main/java/co/casterlabs/caffeinated/builtin/widgets/labels/RecentSubscriberLabel.java
@@ -0,0 +1,100 @@
+package co.casterlabs.caffeinated.builtin.widgets.labels;
+
+import java.io.IOException;
+
+import org.jetbrains.annotations.Nullable;
+
+import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetDetails;
+import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetDetails.WidgetDetailsCategory;
+import co.casterlabs.caffeinated.pluginsdk.widgets.WidgetInstance;
+import co.casterlabs.caffeinated.util.HtmlEscape;
+import co.casterlabs.koi.api.listener.KoiEventHandler;
+import co.casterlabs.koi.api.listener.KoiEventListener;
+import co.casterlabs.koi.api.types.events.SubscriptionEvent;
+import co.casterlabs.koi.api.types.user.User;
+import co.casterlabs.rakurai.json.Rson;
+import co.casterlabs.rakurai.json.element.JsonElement;
+import co.casterlabs.rakurai.json.element.JsonObject;
+import lombok.NonNull;
+
+public class RecentSubscriberLabel extends GenericLabel implements KoiEventListener {
+    public static final WidgetDetails DETAILS = new WidgetDetails()
+        .withNamespace("co.casterlabs.recent_subscriber_label")
+        .withIcon("users")
+        .withCategory(WidgetDetailsCategory.LABELS)
+        .withFriendlyName("Recent Subscriber Label");
+
+    private User recentSubscriber;
+    private String currHtml = "";
+
+    @Override
+    public void onInit() {
+        super.onInit();
+
+        this.addKoiListener(this);
+
+        // If this fails then we don't care.
+        try {
+            JsonElement recentSubscriber = this.getSettings().get("recent_subscriber");
+
+            this.recentSubscriber = Rson.DEFAULT.fromJson(recentSubscriber, User.class);
+        } catch (Exception ignored) {}
+
+        this.updateText();
+    }
+
+    @Override
+    protected void onSettingsUpdate() {
+        this.updateText();
+    }
+
+    @KoiEventHandler
+    public void onSubscription(@Nullable SubscriptionEvent event) {
+        if (event.getGiftRecipient() != null) {
+            this.recentSubscriber = event.getGiftRecipient();
+        } else {
+            this.recentSubscriber = event.getSubscriber();
+        }
+
+        this.getSettings().put("recent_subscriber", Rson.DEFAULT.toJson(this.recentSubscriber));
+
+        this.updateText();
+    }
+
+    private void updateText() {
+        if (this.recentSubscriber != null) {
+            String html = String.valueOf(this.recentSubscriber.getDisplayname());
+            String prefix = HtmlEscape.escapeHtml(this.getSettings().getString("text.prefix")).replace(" ", "&nbsp;");
+            String suffix = HtmlEscape.escapeHtml(this.getSettings().getString("text.suffix")).replace(" ", "&nbsp;");
+
+            if (!prefix.isEmpty()) {
+                html = prefix + ' ' + html;
+            }
+
+            if (!suffix.isEmpty()) {
+                html = html + ' ' + suffix;
+            }
+
+            this.currHtml = html;
+
+            for (WidgetInstance instance : this.getWidgetInstances()) {
+                try {
+                    instance.emit("html", JsonObject.singleton("html", this.currHtml));
+                } catch (IOException ignored) {}
+            }
+        }
+    }
+
+    @Override
+    public void onNewInstance(@NonNull WidgetInstance instance) {
+        try {
+            instance.emit("html", JsonObject.singleton("html", this.currHtml));
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    protected boolean hasHighlight() {
+        return false;
+    }
+
+}
