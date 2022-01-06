cf8170d4abbe8b08c1f823ab73a27db76167d7bb
Ender <33337309+e3ndr@users.noreply.github.com>
2022-01-06 00:06:21 -0600

    App: Cleanup the CEF download progress dialog and move it into it's own file.

--- app/Build/Webview-CEF/src/main/java/co/casterlabs/caffeinated/bootstrap/webview/impl/CefUtil.java
+++ app/Build/Webview-CEF/src/main/java/co/casterlabs/caffeinated/bootstrap/webview/impl/CefUtil.java
@@ -1,13 +1,6 @@
 package co.casterlabs.caffeinated.bootstrap.webview.impl;
 
-import java.awt.BorderLayout;
 import java.io.File;
-import java.net.URL;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JProgressBar;
 
 import org.cef.CefApp;
 import org.cef.CefClient;
@@ -20,18 +13,11 @@ import org.cef.handler.CefResourceHandler;
 import org.cef.network.CefRequest;
 
 import co.casterlabs.caffeinated.app.CaffeinatedApp;
-import co.casterlabs.caffeinated.bootstrap.FileUtil;
 import co.casterlabs.caffeinated.bootstrap.webview.scheme.SchemeHandler;
-import co.casterlabs.caffeinated.util.DualConsumer;
-import co.casterlabs.caffeinated.util.Pair;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import me.friwi.jcefmaven.CefAppBuilder;
-import me.friwi.jcefmaven.EnumProgress;
-import me.friwi.jcefmaven.IProgressHandler;
 import me.friwi.jcefmaven.MavenCefAppHandlerAdapter;
-import xyz.e3ndr.consoleutil.consolewindow.BarStyle;
-import xyz.e3ndr.fastloggingframework.logging.FastLogger;
 
 public class CefUtil {
     public static final boolean enableOSR = System.getProperty("caffeinated.cef.offscreenrendering.enable", "").equals("true"); // Defaults to false
@@ -83,45 +69,7 @@ public class CefUtil {
 
             });
 
-            builder.setProgressHandler(new IProgressHandler() {
-                private FastLogger logger = new FastLogger("Cef Bundle");
-
-                private DualConsumer<String, Integer> progressHandler;
-                private JFrame progressFrame;
-
-                @Override
-                public void handleProgress(EnumProgress state, float percent) {
-                    if (percent < 0) {
-                        this.logger.info(
-                            "%-13s",
-                            state
-                        );
-                    } else {
-                        this.logger.info(
-                            "%-13s %s",
-                            state,
-                            BarStyle.ANGLE.format(percent / 100, 30, true)
-                        );
-                    }
-
-                    if ((state == EnumProgress.DOWNLOADING) && (progressFrame == null)) {
-                        Pair<JFrame, DualConsumer<String, Integer>> downloadDialog = createDownloadDialog();
-
-                        this.progressFrame = downloadDialog.a;
-                        this.progressHandler = downloadDialog.b;
-                    }
-
-                    if (progressFrame != null) {
-                        if (state == EnumProgress.INITIALIZED) {
-                            this.progressFrame.dispose();
-                            this.progressFrame = null;
-                            this.progressHandler = null;
-                        } else {
-                            this.progressHandler.accept(state.name(), (int) percent);
-                        }
-                    }
-                }
-            });
+            builder.setProgressHandler(new CefDownloadProgressDialog());
 
             builder.build();
         } catch (Throwable t) {
@@ -132,44 +80,6 @@ public class CefUtil {
     @SneakyThrows
     public static CefClient createCefClient() {
         return CefApp.getInstance().createClient();
-    }
-
-    @SneakyThrows
-    private static Pair<JFrame, DualConsumer<String, Integer>> createDownloadDialog() {
-        JFrame frame = new JFrame();
-
-        frame.setLayout(new BorderLayout(0, 0));
-        frame.setVisible(true);
-
-        // Setup progress bar
-        JProgressBar progressBar = new JProgressBar(0, 100);
-
-        progressBar.setValue(0);
-        progressBar.setStringPainted(true);
-
-        frame.getContentPane().add(progressBar, BorderLayout.CENTER);
-
-        // The state text
-        JLabel stateText = new JLabel();
-
-        frame.getContentPane().add(stateText, BorderLayout.NORTH);
-
-        // Set the icon.
-        {
-            URL iconUrl = FileUtil.loadResourceAsUrl("assets/logo/casterlabs.png");
-            ImageIcon img = new ImageIcon(iconUrl);
-
-            frame.setIconImage(img.getImage());
-        }
-
-        DualConsumer<String, Integer> progressConsumer = (state, progress) -> {
-            progressBar.setVisible(progress > 0);
-            progressBar.setValue(progress);
-
-            stateText.setText(state);
-        };
-
-        return new Pair<>(frame, progressConsumer);
     }
 
 }
