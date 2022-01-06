118fc97b7216f1f9c1badd91348fd6cab0dceaab
Ender <33337309+e3ndr@users.noreply.github.com>
2022-01-06 03:19:44 -0600

    App: Linux now has system playback support! (Via playerctl)

--- app/Build/Caffeinated-Linux/src/main/java/co/casterlabs/caffeinated/bootstrap/linux/LinuxBootstrap.java
+++ app/Build/Caffeinated-Linux/src/main/java/co/casterlabs/caffeinated/bootstrap/linux/LinuxBootstrap.java
@@ -2,17 +2,25 @@ package co.casterlabs.caffeinated.bootstrap.linux;
 
 import co.casterlabs.caffeinated.bootstrap.Bootstrap;
 import co.casterlabs.caffeinated.bootstrap.NativeSystemProvider;
+import co.casterlabs.caffeinated.bootstrap.linux.music.LinuxSystemPlaybackMusicProvider;
 import co.casterlabs.caffeinated.bootstrap.webview.impl.CefWebview;
 
 public class LinuxBootstrap {
 
     @SuppressWarnings("deprecation")
     public static void main(String[] args) throws Exception {
+        LinuxSystemPlaybackMusicProvider playbackProvider = null;
+
+        if (LinuxSystemPlaybackMusicProvider.isPlayerCtlInstalled()) {
+            playbackProvider = new LinuxSystemPlaybackMusicProvider();
+        }
+
         NativeSystemProvider.initialize(
             null,
-            null,
+            playbackProvider,
             CefWebview.FACTORY
         );
+
         Bootstrap.main(args);
     }
 
