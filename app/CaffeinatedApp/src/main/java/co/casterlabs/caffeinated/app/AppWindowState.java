fb0126483f4476857c2de6d3935e98af65ca1e38
Ender <33337309+e3ndr@users.noreply.github.com>
2022-02-02 22:21:41 -0600

    App: Fix the app's icon.

--- app/CaffeinatedApp/src/main/java/co/casterlabs/caffeinated/app/AppWindowState.java
+++ app/CaffeinatedApp/src/main/java/co/casterlabs/caffeinated/app/AppWindowState.java
@@ -1,9 +1,20 @@
 package co.casterlabs.caffeinated.app;
 
 import co.casterlabs.caffeinated.util.async.AsyncTask;
+import co.casterlabs.caffeinated.webview.Webview;
+import co.casterlabs.caffeinated.webview.WebviewFactory;
 import co.casterlabs.caffeinated.webview.WebviewWindowState;
+import co.casterlabs.rakurai.json.annotating.JsonSerializationMethod;
+import co.casterlabs.rakurai.json.element.JsonElement;
+import co.casterlabs.rakurai.json.element.JsonString;
 
 public class AppWindowState extends WebviewWindowState {
+
+    @JsonSerializationMethod("icon")
+    private JsonElement $serialize_icon() {
+        Webview.getWebviewFactory();
+        return new JsonString(WebviewFactory.getCurrentIcon());
+    }
 
     @Override
     public void update() {
