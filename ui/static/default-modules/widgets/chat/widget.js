2c9da8d3203edf62c90743fb336a8aaf4e3195a6
Ender <33337309+e3ndr@users.noreply.github.com>
2022-02-03 16:33:47 -0600

    UI: Remove an old folder.

--- ui/static/default-modules/widgets/chat/widget.js
+++
@@ -1,7 +0,0 @@
-
-Module.on("init", ({ name, id, namespace }) => {
-    console.debug("My name:", name);
-    console.debug("Location:", import.meta.url)
-    console.debug(Caffeinated)
-    // alert("Hello from the Chat Widget!")
-});
