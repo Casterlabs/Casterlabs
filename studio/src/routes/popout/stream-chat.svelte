e5dc7d99e93f363d53b169a5b76b00350963e99b
Ender <33337309+e3ndr@users.noreply.github.com>
2022-04-26 14:03:31 -0500

    Caffeinated: Don't reprocess all catchup events.
    Also fixed links in the dock.

--- studio/src/routes/popout/stream-chat.svelte
+++ studio/src/routes/popout/stream-chat.svelte
@@ -170,6 +170,28 @@
         }
 
         conn.init({ initHandler, disconnectHandler });
+
+        // Intercept all anchors and send the open via Caffeinated
+        new MutationObserver(() => {
+            for (const anchor of document.querySelectorAll("a")) {
+                // Intercept all rel="external" and target="_blank" anchors and add a click listener.
+                if ((anchor.rel == "external" || anchor.target == "_blank") && !anchor.getAttribute("dest")) {
+                    anchor.setAttribute("dest", anchor.href);
+                    anchor.href = "#"; // Clear the property incase it tries to open the link.
+
+                    // On click we prevent the navigation and openLink with the href.
+                    // We use the onclick function rather than
+                    // addEventListener("click", ...) because it helps prevent duplicates.
+                    anchor.onclick = () => {
+                        Widget.emit("openLink", anchor.getAttribute("dest"));
+                        return false;
+                    };
+                }
+            }
+        }).observe(document.body, {
+            subtree: true,
+            childList: true
+        });
     });
 </script>
 
