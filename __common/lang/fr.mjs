21ee6bd771a665265ed2e7346337e2fa106dc07a
Ender <33337309+e3ndr@users.noreply.github.com>
2022-04-27 17:22:16 -0500

    Get the French localization up to snuff.

--- __common/lang/fr.mjs
+++ __common/lang/fr.mjs
@@ -7,8 +7,7 @@ export default {
     "app.terms_of_service": "Conditions d'utilisation",
     "app.privacy_policy": "Politique de confidentialité",
     "app.footer": "Fait avec ♥ par Casterlabs.<br/>Copyright © {year} Casterlabs. Tous les droits sont réservés.",
-    "app.service_disclaimer":
-        `
+    "app.service_disclaimer": `
         En vous connectant, vous acceptez nos
         <a href="https://casterlabs.co/terms-of-service" class="fade-on-hover" rel="external">conditions d'utilisation</a>
         et reconnaissez que notre
@@ -33,6 +32,8 @@ export default {
     "generic.yes": "Oui",
     "generic.no": "Non",
     "generic.next": "Suivant",
+    "generic.leftquote": '«',
+    "generic.rightquote": '»',
 
     /* ------------------------ */
     /* Settings                 */
@@ -80,4 +81,64 @@ export default {
     "settings.pair_a_device.success.description": "Vous avez réussi à vous associer à {deviceType}.",
     "settings.pair_a_device.again": "Associer un autre?",
 
-}
+    "sidebar.home": "Accueil",
+    "sidebar.your_stream": "Votre flux",
+    "sidebar.management": "La gestion",
+
+    "chatbot_manager": "Chat Bot",
+    "chatbot_manager.tab.COMMANDS": "COMMANDES",
+    "chatbot_manager.tab.SHOUTS": "DES CRIS",
+    "chatbot_manager.tab.TIMERS": "MINUTEURS",
+    "chatbot_manager.commands.mention": "Mentionner l'expéditeur lors de la réponse: %checkbox%",
+    "chatbot_manager.commands.runs": "exécuter",
+    "chatbot_manager.commands.mentions": "mentionne",
+    "chatbot_manager.commands.format.COMMAND": "Quand quelqu'un de %platform% %action% <b>!</b>%action_target%, envoie: %message%",
+    "chatbot_manager.commands.format.CONTAINS": "Quand quelqu'un de %platform% %action% <b>[generic.leftquote]</b>%action_target%<b>[generic.rightquote]</b>, répondez avec: %message%",
+    "chatbot_manager.shouts.format": "Quand quelqu'un de %platform% %action% dit: %message%",
+    "chatbot_manager.shouts.DONATION": "fait un don",
+    "chatbot_manager.shouts.FOLLOW": "suit",
+    "chatbot_manager.shouts.RAID": "raids",
+    "chatbot_manager.shouts.SUBSCRIPTION": "s'abonne",
+    "chatbot_manager.timers.format": "Toutes les %seconds% secondes, envoyez l'un des éléments suivants:",
+    "chatbot_manager.example.command": "Découvrez casterlabs.co!",
+    "chatbot_manager.example.shout": "Merci pour l'abonnement!",
+    "chatbot_manager.example.timer": "J'adore Casterlabs!",
+
+    "widget_manager": "Widgets",
+    "dock_manager": "Quais",
+
+    "chat.viewer": "Discuter",
+    "chat.viewer.send_message": "Envoyer",
+    "chat.viewer.send_message.placeholder": "Envoyer un message",
+    "chat.viewer.preferences.title": "Préférences de chat",
+    "chat.viewer.preferences.show_timestamps": "Afficher les horodatages",
+    "chat.viewer.preferences.show_mod_actions": "Afficher les actions de mod",
+    "chat.viewer.preferences.show_avatars": "Afficher les avatars",
+    "chat.viewer.preferences.show_badges": "Afficher les badges",
+    "chat.viewer.preferences.show_viewers": "Afficher les spectateurs",
+    "chat.viewer.preferences.show_viewers_list": "Afficher la liste des spectateurs",
+
+    "chat.message.cleared": "Le chat a été effacé",
+    "chat.message.channelpoints": "{displayname} vient d'échanger {imageHtml}{reward}",
+    "chat.message.raid": "{displayname} vient de faire un raid avec {viewers} spectateurs",
+    "chat.message.follow": "{displayname} vient de suivre",
+    "chat.message.sub.SUB": "{displayname} vient de s'abonner pour {months} mois",
+    "chat.message.sub.RESUB": "{displayname} vient de se réabonner pour {months} mois",
+    "chat.message.sub.SUBGIFT": "{gifter} vient d'offrir à {recipient} un abonnement de {months} mois",
+    "chat.message.sub.ANONSUBGIFT": "Anonymous vient d'offrir à {recipient} un abonnement de {months} mois",
+    "chat.message.viewer.join": "{displayname} a rejoint",
+    "chat.message.viewer.leave": "{displayname} a quitté",
+
+    "chat.action.ban": "Interdire",
+    "chat.action.timeout": "Temps libre",
+    "chat.action.delete_message": "Supprimer le message",
+
+    "widgets.manager": "Widgets",
+    "widgets.manager.alerts": "Alertes",
+    "widgets.manager.labels": "Étiquettes",
+    "widgets.manager.interaction": "Interaction",
+    "widgets.manager.goals": "Objectifs",
+    "widgets.manager.other": "Autre",
+    "widgets.editor": "Éditeur de widget",
+    "widgets.editor.preview": "Aperçu"
+};
