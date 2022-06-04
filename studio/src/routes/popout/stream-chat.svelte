05bfa4891641dcd341dac23577b860b5f1dcb4bf
Ender <33337309+e3ndr@users.noreply.github.com>
2022-06-04 17:24:37 -0500

    Caffeinated (Popouts): Cleanup the popout code.
    This is for the rework of the YouTube queue.

--- studio/src/routes/popout/stream-chat.svelte
+++ studio/src/routes/popout/stream-chat.svelte
@@ -1,10 +1,5 @@
 <script>
-    import { onMount } from "svelte";
-
-    import LoadingSpinner from "$lib/components/LoadingSpinner.svelte";
     import ChatViewer from "$lib/components/chat/chat-viewer.svelte";
-
-    let isConnected = false;
 
     let viewerElement = {};
 
@@ -149,64 +144,6 @@
 
         bridge_onAuthUpdate(Object.keys(Koi.userStates));
     }
-
-    onMount(async () => {
-        document.title = "Casterlabs Caffeinated - Stream Chat";
-
-        const conn = await import("https://widgets.casterlabs.co/caffeinated/widget.mjs");
-
-        function initHandler() {
-            isConnected = true;
-            return true;
-        }
-
-        function disconnectHandler() {
-            setTimeout(() => {
-                conn.init({ initHandler, disconnectHandler });
-            }, 7500);
-            isConnected = false;
-            return false;
-        }
-
-        conn.init({ initHandler, disconnectHandler });
-
-        // Intercept all anchors and send the open via Caffeinated
-        new MutationObserver(() => {
-            for (const anchor of document.querySelectorAll("a")) {
-                // Intercept all rel="external" and target="_blank" anchors and add a click listener.
-                if ((anchor.rel == "external" || anchor.target == "_blank") && !anchor.getAttribute("dest")) {
-                    anchor.setAttribute("dest", anchor.href);
-                    anchor.href = "#"; // Clear the property incase it tries to open the link.
-
-                    // On click we prevent the navigation and openLink with the href.
-                    // We use the onclick function rather than
-                    // addEventListener("click", ...) because it helps prevent duplicates.
-                    anchor.onclick = () => {
-                        Widget.emit("openLink", anchor.getAttribute("dest"));
-                        return false;
-                    };
-                }
-            }
-        }).observe(document.body, {
-            subtree: true,
-            childList: true
-        });
-    });
 </script>
 
-{#if isConnected}
-    <ChatViewer bind:this={viewerElement} on:chatsend={onChatSend} on:modaction={onModAction} on:savepreferences={onSavePreferences} on:mount={initViewer} />
-{:else}
-    <div class="loading-spinner">
-        <LoadingSpinner />
-    </div>
-{/if}
-
-<style>
-    .loading-spinner {
-        margin: auto;
-        margin-top: 125px;
-        width: 50px;
-        height: 50px;
-    }
-</style>
+<ChatViewer bind:this={viewerElement} on:chatsend={onChatSend} on:modaction={onModAction} on:savepreferences={onSavePreferences} on:mount={initViewer} />
