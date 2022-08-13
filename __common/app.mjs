f9d39ad6f2ad60f03f5ebb9c53df94fa18d1ac4e
Ender <33337309+e3ndr@users.noreply.github.com>
2022-08-13 00:29:08 -0500

    Common: Emoji helper for the next commit :^)

--- __common/app.mjs
+++ __common/app.mjs
@@ -1,15 +1,41 @@
-import EventHandler from './event-handler.mjs';
+import EventHandler from "./event-handler.mjs";
 
 const eventHandler = new EventHandler();
 
 let store = {
-    "language": "en-US",
-    "emojiProvider": "system",
-    "theme": "co.casterlabs.dark"
+    language: "en-US",
+    emojiProvider: "system",
+    theme: "co.casterlabs.dark"
 };
 
-export default {
+let appStyleElement;
 
+function recomputeStyle() {
+    const { emojiProvider } = store;
+
+    if (emojiProvider == "system") {
+        appStyleElement.innerHTML = ``;
+    } else {
+        appStyleElement.innerHTML = `
+        [data-rich-type="emoji"] > [data-emoji-provider="system"] {
+            display: none;
+        }
+        
+        [data-rich-type="emoji"] > [data-emoji-provider="${emojiProvider}"] {
+            display: inline-block !important;
+        }
+        `;
+    }
+}
+
+function init() {
+    appStyleElement = document.createElement("style");
+    appStyleElement.id = "app-style";
+    document.head.appendChild(appStyleElement);
+    recomputeStyle();
+}
+
+export default {
     store() {
         return store;
     },
@@ -31,10 +57,17 @@ export default {
     },
 
     mutate(key, value) {
+        if (!appStyleElement) {
+            init();
+        }
+
         if (store[key] !== value) {
             store[key] = value;
             eventHandler.emit(key, value);
+
+            if (["emojiProvider"].includes(key)) {
+                recomputeStyle();
+            }
         }
     }
-
 };
