e1f8968b125a444d389f7a546ef549b95fbe197e
Ender <33337309+e3ndr@users.noreply.github.com>
2022-11-05 14:24:26 -0500

    Caffeinated: Use the new sync program :^)

--- __common/sync/main.mjs
+++ __common/sync/main.mjs
@@ -42,27 +42,27 @@ for (const directory of DIRECTORIES) {
     watcher
         .on("add", (path) => {
             path = path.substring(directory.length);
-            // console.debug("File added:", directory, path);
+            console.debug("File added:", directory, path);
             syncChange(directory, path);
         })
         .on("unlink", (path) => {
             path = path.substring(directory.length);
-            // console.debug("File deleted:", directory, path);
+            console.debug("File deleted:", directory, path);
             syncUnlink(directory, path);
         })
         .on("change", (path) => {
             path = path.substring(directory.length);
-            // console.debug("File changed:", directory, path);
+            console.debug("File changed:", directory, path);
             syncChange(directory, path);
         })
         .on("addDir", (path) => {
             path = path.substring(directory.length);
-            // console.debug("Directory added:", directory, path);
+            console.debug("Directory added:", directory, path);
             syncDirAdd(directory, path);
         })
         .on("unlinkDir", (path) => {
             path = path.substring(directory.length);
-            // console.debug("Directory deleted:", directory, path);
+            console.debug("Directory deleted:", directory, path);
             syncDirUnlink(directory, path);
         })
         .on("error", (error) => {
