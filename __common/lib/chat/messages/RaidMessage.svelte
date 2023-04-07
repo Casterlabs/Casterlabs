c7bdc87964a96781c904c8c09ce7efdec4e3c797
Ender <33337309+e3ndr@users.noreply.github.com>
2023-04-07 01:01:59 -0500

    Common: Use badges instead of (Parenthesis)

---
+++ __common/lib/chat/messages/RaidMessage.svelte
@@ -0,0 +1,16 @@
+<script>
+	import LocalizedText from '$lib/LocalizedText.svelte';
+	import { t } from '$lib/translate.mjs';
+
+	export let event;
+	export let onContextMenuAction;
+</script>
+
+<LocalizedText
+	key="chat.viewer.message.RAID"
+	opts={{
+		displayname: event.host.displayname,
+		viewers: event.viewers,
+		platform: event.streamer.platform.toLowerCase()
+	}}
+/>
